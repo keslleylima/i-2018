f0444cde198b8c7d2f053a79f2ea7b4e73418388
Keslley L <keslley_14_20@hotmail.com>
2018-03-25 21:32:49 -0300

    add exercício 1

---
+++ Manipulação básica de arquivos/ManipulaçãoDeArquivos/src/manipulaçãodearquivos/Exercicio1.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package manipulaçãodearquivos;
+
+import java.util.Scanner;
+import java.io.FileInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+
+/**
+ *
+ * @author keslley
+ */
+public class Exercicio1 {
+
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Insira o nome do arquivo:");
+        String ArquivoNome = leitura.nextLine();
+          	
+	    try {
+	    	FileInputStream novoArquivo = new FileInputStream(ArquivoNome);
+                int valorSaida;
+            try (DataInputStream dataIn = new DataInputStream(novoArquivo)) {
+                valorSaida = dataIn.readInt();
+            }
+	    	
+	    	System.out.println(Integer.toHexString(valorSaida));
+	    	
+	    } catch (IOException excecao) {
+	        System.out.println("Desculpe, houve um erro. Tente novamente");
+	          excecao.getMessage();
+	    }
+    }
+
+}
