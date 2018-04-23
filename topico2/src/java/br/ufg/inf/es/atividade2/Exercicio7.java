1413b775cef2fc70b79f4400642679a304ac1126
Keslley L <keslley_14_20@hotmail.com>
2018-04-22 23:03:54 -0300

    add exerc 7

---
+++ topico2/src/java/br/ufg/inf/es/atividade2/Exercicio7.java
@@ -0,0 +1,43 @@
+package br.ufg.inf.es.atividade2;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.file.Files;
+
+/**
+ * Created by aluno on 16/04/18.
+ */
+public class Exercicio7 {
+
+    public void RecuperaAvatar() {
+        try {
+            URL url = new URL(https://api.github.com/users/kyriosdata);
+            HttpURLConnection conexao = (HttpURLConnection) url.openConnection();
+
+            conexao.setRequestMethod("GET");
+            conexao.connect();
+
+            leitor = new BufferedReader(new InputStreamReader(conexao.getInputStream()));
+            StringBuilder tmp = new StringBuilder();
+            String linnha = "";
+
+            while (linha = leitor.readLine()) {
+                tmp.append(line + "\n");
+            }
+
+            String tmp = tmp.toString();
+
+            JsonParser parser = new JsonParser();
+            JsonObject gitUser = parser.parse(tmp).getAsJsonObject();
+            String avatar = gitUser.get("avatar_url").toString().replaceAll("\"", "");
+            //Files.copy(avatar);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
