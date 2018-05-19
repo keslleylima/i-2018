25d62dd60e6b1c131bf825e6062cde04911564e2
keslleylima <keslley_14_20@hotmail.com>
2018-05-19 02:15:22 -0300

    Ajustando exercício 1 para passar os args e não ser hard code

---
+++ topico2/src/main/java/br/ufg/inf/es/integracao/serelizacao/exercicio1/Cliente.java
@@ -0,0 +1,28 @@
+package topico2.src.main.java.br.ufg.inf.es.integracao.serelizacao.exercicio1;
+
+public class Cliente {
+
+    String nome;
+    String identificador;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getIdentificador() {
+        return identificador;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdentificador(String identificador) {
+        this.identificador = identificador;
+    }
+
+    public Cliente(String nome, String identificador) {
+        this.nome = nome;
+        this.identificador = identificador;
+    }
+}
---
+++ topico2/src/main/java/br/ufg/inf/es/integracao/serelizacao/exercicio2/Cliente.java
@@ -0,0 +1,4 @@
+package topico2.src.main.java.br.ufg.inf.es.integracao.serelizacao.exercicio2;
+
+public class Cliente {
+}
